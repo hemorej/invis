5b3621b923f21a45797f795ebc59affe037a51fa
j_a_ <jerome.xa88@gmail.com>
2015-12-26 08:43:21 -0500

    dummy accounts config

--- site/accounts/jerome.php
+++ site/accounts/jerome.php
@@ -3,6 +3,9 @@
 username: jerome
 email: jerome.xa88@gmail.com
 password: >
-  $2a$10$dT6klDkvfGcE.4KBEPV8Guy5yCO/YHjeJKPopCla5atb./kDGIVgW
+  $2a$10$79x1JCD93n4xps4ErLd1Nur2paDgKnCuXFkwZuourz7qXEUg69L7a
 language: en
 role: admin
+token: 3d3df35541e5f9016fab2efe941776c3c336f122
+firstname: J
+lastname: A
