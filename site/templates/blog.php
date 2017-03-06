d600e393fbcb09429cd73fd0244df14cc78892b1
hemorej <hemorej@users.noreply.github.com>
2017-03-06 10:36:38 -0500

    blog: speed up loading newest entry

--- site/templates/blog.php
+++ site/templates/blog.php
@@ -60,7 +60,7 @@ if(isset($_GET['archive'])){
     <?php snippet('footer') ?>
 
 <?php }else{
-    $article = $page->children()->visible()->flip()->first();
+    $article = $page->children()->last();
     echo go($article->url());
 }
 ?>
