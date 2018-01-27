9a53f23773efa28aee3d4efb7b419c23ae002dc4
j_a_ <jerome.xa88@gmail.com>
2018-01-26 19:04:21 -0500

    articles/products code refactoring

--- site/templates/articles.php
+++ site/templates/articles.php
@@ -4,19 +4,22 @@
 <?php $articles = $page->children()->visible()->sortBy('publishDate', 'desc'); ?>
 
 <div class="row medium-space-top">
-	<div class="medium-12 columns">
-
-		<ul class="inline-list">
-			<?php foreach($articles as $article): ?>
-				<li class="date centre title"><a href="<?= $article->url() ?>"><?= $article->title()->lower() ?></a></li>
-				<?php foreach($article->images()->slice(0, rand(2,4)) as $image): ?>
-				<li>
-					<a class="thumb" style="background-image:url(<?= thumb($image, array('height' => 150, 'width' => 150, 'crop' => true))->url(); ?>)"  href="<?= $article->url() ?>"></a>
-				</li>
-				<?php endforeach ?>
-			<?php endforeach ?>
-		</ul>
-	</div>
+    <div class="small-12 medium-12 columns">
+        <ul class="inline-list">
+            <?php foreach($articles as $article): ?>
+                <?php if(strtolower($page->title()->value()) == 'prints'): ?>
+                    <li class="date"><?= $article->title()->lower() ?></li>
+                <?php else: ?>
+                    <li class="date centre title"><a href="<?= $article->url() ?>"><?= $article->title()->lower() ?></a></li>
+                <?php endif ?>
+                <?php foreach($article->images()->slice(0, rand(2,4)) as $image): ?>
+                <li>
+                    <a class="thumb" style="background-image:url(<?= thumb($image, array('height' => 150, 'width' => 150, 'crop' => true))->url(); ?>)"  href="<?= $article->url() ?>"></a>
+                </li>
+                <?php endforeach ?>
+            <?php endforeach ?>
+        </ul>
+    </div>
 </div>
 
 <?php snippet('footer') ?>
