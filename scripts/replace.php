aefb835357d69134cd34877ac8165b12d928bc3a
j_a_ <jerome.xa88@gmail.com>
2018-02-01 21:50:09 -0500

    fix script

--- scripts/replace.php
+++ scripts/replace.php
@@ -3,21 +3,46 @@
 $dirs = array('01-travels', '02-projects', '04-journal');
 
 foreach($dirs as $dir){
-	$cmd = 'find ' . $dir . ' -type f -iname "*.jpg"';
+	$files = array();
+	$cmd = 'find /Applications/MAMP/htdocs/invis/content/' . $dir . ' -type f -iname "*.jpg"';
 	exec($cmd, $files);
 
 	foreach($files as $file){
-		$original = '';
-		$filename = preg_replace('/^(hr\.|\d\d_)/', '', basename($file));
-		$cmd = 'find ~/Pictures/Photography -type f -iname "' . $filename . '*" | grep -vi original';
-		exec($cmd, $original);
+		if(strstr($file, 'discard') != false)
+			continue;
 
-		if(empty($original[0])){
-			echo basename(dirname($file)) ."/$filename not found!\n";
+		$filename = preg_replace('/^(hr\.|\d{2}_|\d{2}-)/', '', basename($file));
+		$filename = preg_replace('/(__\d).jpg$/', '.jpg', basename($filename));
+		$filename = preg_replace('/^(m6_)/', 'm6-', basename($filename));
+		$filename = preg_replace('/_small/', '', basename($filename));
+		$original = findFile($filename);
+
+		if(empty($original)){
+			$filename = preg_replace('/-(\d{2,3})/', '_$1', $filename);
+			$original = findFile($filename);
+
+			if(empty($original)){
+				$original = findFile($filename, '/Volumes/Backup/Photography');
+				if(empty($original)){
+					echo basename(dirname($file)) ."/$filename not found!\n";
+				}else{
+					rename($file, $file.".bak");
+					copy($original, dirname($file)."/$filename");
+				}
+			}else{
+				rename($file, $file.".bak");
+				copy($original, dirname($file)."/$filename");
+			}
 		}else{
 			rename($file, $file.".bak");
-			copy($original[0], dirname($file)."/$filename");
+			copy($original, dirname($file)."/$filename");
 		}
 	}
+}
 
+function findFile($name, $path='~/Pictures/Photography'){
+	$cmd = 'find ' . $path . ' -type f -iname "' . $name . '*" | grep -vi original';
+	exec($cmd, $original);
+
+	return $original[0];
 }
