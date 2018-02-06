dde15af9edcc9a0cb9bbe6f69d4ac8647bc5753a
j_a_ <jerome.xa88@gmail.com>
2018-02-06 18:21:45 -0500

    design preview class for mobile

--- site/templates/articles.php
+++ site/templates/articles.php
@@ -6,12 +6,12 @@
 <div class="row large-space-top"></div>
 <?php foreach($articles as $article): ?>
 <div class="row">
-    <div class="small-12 medium-4 medium-text-right columns">
+    <div class="small-5 medium-4 text-right columns">
         <h3><a data-preview="<?= getPreview($article->images()->first()) ?>" class="cover" href="<?= $article->url() ?>"><?= html($article->title()->lower()) ?></a></h3>
     </div>
 </div>
 <?php endforeach ?>
-<div class="preview hide-for-small">
+<div class="preview">
     <img id="cover" src="<?= getPreview($articles->first()->images()->first()) ?>" >
 </div>
 
