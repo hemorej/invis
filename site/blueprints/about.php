81356f06cb36756589fe4eadd8c18c8baa2fa46c
j_a_ <jerome.arfouche@gmail.com>
2018-10-24 18:43:33 -0400

    update about page blueprint

---
+++ site/blueprints/about.php
@@ -0,0 +1,18 @@
+title: About
+pages: false
+files: true
+fields:
+	text:
+		label: Text
+		type: textarea
+		size: large
+		required: true    
+	links:
+		label: Links
+		type: textarea
+		size: large
+		required: true
+	contact:
+		label: Your content
+		type: textarea
+		size: large
