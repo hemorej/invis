34f4347d9c6a7133ed674576893545ceefeefe8a
j_a_ <jerome.arfouche@gmail.com>
2019-12-02 10:14:56 -0500

    convert order page to tachyons

--- site/templates/order.blade.php
+++ site/templates/order.blade.php
@@ -5,15 +5,13 @@
 </script>
 
 @if($state == 'complete')
-	<div class="row medium-space-top">
-    	<span class="high-contrast">Order confirmation</span>
+	<div class="measure black-70 f4 f4-ns f3-l ph2 mt4">
+    	<h4>Order confirmation</h4>
 	</div>
-	<span></span>
-	<div class="row">
-		<div class="small-12 medium-12 columns">
-			<h2>Thank you for your order</h2>
-			Your order number is {{ $order->autoid() }}. You will receive an email confirmation shortly. If you have questions about your order or would like to make changes contact us at &#x6A;&#x65;&#x72;&#x6F;&#x6D;&#x65;&#x40;&#x74;&#x68;&#x65;&#x2D;&#x69;&#x6E;&#x76;&#x69;&#x73;&#x69;&#x62;&#x6C;&#x65;&#x2D;&#x63;&#x69;&#x74;&#x69;&#x65;&#x73;&#x2E;&#x63;&#x6F;&#x6D;
-		</div>
+	<span class='db mb2'></span>
+	<div class="black-70 f4 f4-ns f3-ns ph2">
+		<span class="measure lh-copy db">Thank you for your order</span>
+		Your order number is {{ $order->autoid() }}. You will receive an email confirmation shortly. If you have questions about your order or would like to make changes contact us at &#x6A;&#x65;&#x72;&#x6F;&#x6D;&#x65;&#x40;&#x74;&#x68;&#x65;&#x2D;&#x69;&#x6E;&#x76;&#x69;&#x73;&#x69;&#x62;&#x6C;&#x65;&#x2D;&#x63;&#x69;&#x74;&#x69;&#x65;&#x73;&#x2E;&#x63;&#x6F;&#x6D;
 	</div>
 
 	@php
@@ -26,9 +24,10 @@
         }
     @endphp
 
-	<div class="row medium-space-top">
-		<div class="small-12 medium-6 columns">
-			<h2>Order details</h2>
+    <section class="mw9 center ph2 black-70 f4 f4-ns">
+	  <div class="cf">
+	    <div class="fl w-100 w-70-l">
+	      <h3>Order details</h3>
 			<table>
         	@foreach($items as $item)
               <tr>
@@ -45,29 +44,28 @@
                 <td>— <b>${{ intval($total) }}</b></td>
               </tr>
           </table>
-		</div>
-		<div class="small-12 medium-6 columns">
-			<h2>Shipping details</h2>
+	    </div>
+	    <div class="fl w-100 w-30-l ph2-l">
+	    	<h3>Shipping details</h3>
 			@php
 				$customer = \Yaml::decode($order->customer());
 				$address = $customer['address'];
 			@endphp
 
 			{{ $customer['name'] }}<br/> {{ $address['address_line_1'] }} {{ $address['address_line_2'] }}<br /> {{ $address['city'] }}, {{ $address['state'] }} <br /> {{ $address['country'] }}<br/> {{ $address['postal_code'] }}<br/> {{ $customer['email'] }}<br/>
-		</div>
-	</div>
+	    </div>
+	  </div>
+	</section>
 	
 @include('partials.footer')
 @elseif($state == 'error')
-	<div class="row medium-space-top">
-    	<span class="high-contrast">Order error</span>
+	<div class="measure black-70 f4 f4-ns f3-l ph2 mt4">
+    	<span class="f4 f4-ns f3-l measure lh-copy db">Order error</span>
 	</div>
-	<span></span>
-	<div class="row">
-		<div class="small-12 medium-12 columns">
-			I'm sorry but there's a problem with your order. {{ $message }}<br />
-			Please contact me with your session ID ({{ $kirby->session()->startTime() . $kirby->session()->expiryTime()}}) at &#x6A;&#x65;&#x72;&#x6F;&#x6D;&#x65;&#x40;&#x74;&#x68;&#x65;&#x2D;&#x69;&#x6E;&#x76;&#x69;&#x73;&#x69;&#x62;&#x6C;&#x65;&#x2D;&#x63;&#x69;&#x74;&#x69;&#x65;&#x73;&#x2E;&#x63;&#x6F;&#x6D; to resolve this issue. 
-		</div>
+	<span class='db mb2'></span>
+	<div class="measure black-70 f4 f4-ns f3-l ph2 mt4">
+		I'm sorry but there's a problem with your order. {{ $message }}<br />
+		Please contact me with your session ID ({{ $kirby->session()->startTime() . $kirby->session()->expiryTime()}}) at &#x6A;&#x65;&#x72;&#x6F;&#x6D;&#x65;&#x40;&#x74;&#x68;&#x65;&#x2D;&#x69;&#x6E;&#x76;&#x69;&#x73;&#x69;&#x62;&#x6C;&#x65;&#x2D;&#x63;&#x69;&#x74;&#x69;&#x65;&#x73;&#x2E;&#x63;&#x6F;&#x6D; to resolve this issue. 
 	</div>
 @else
 	{{ @go('/prints') }}
