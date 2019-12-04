b7ccbd6047c438748cc870a3ebdea9a1b7ef3539
j_a_ <jerome.arfouche@gmail.com>
2019-12-04 08:36:46 -0500

    small fix for cookie consent and ga

--- site/templates/partials/ga.blade.php
+++ site/templates/partials/ga.blade.php
@@ -3,16 +3,16 @@
 @if($loc->location->is_eu == true)
 	@css('//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.1.0/cookieconsent.min.css')
 	@js('//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.1.0/cookieconsent.min.js')
-	@js('assets/js/prod/consent.min.js', ['id' => 'consent', 'data-loc' => $loc, 'data-ga' => option('ga_code')])
-@else
-	@if(!empty(@option('ga_code')))
+	@js('assets/js/consent.js', ['id' => 'consent', 'data-loc' => $loc->country_code, 'data-ga' => option('ga_code')])
+@else 
+	@if(!empty(option('ga_code')))
 		<script>
 		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 		  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-		  ga('create', '<?= option('ga_code') ?>', 'auto');
+		  ga('create', '{{ option('ga_code') }}', 'auto');
 		  ga('send', 'pageview');
 
 		</script>
