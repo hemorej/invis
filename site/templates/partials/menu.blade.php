78630ef64d89f59a1fcf65297a08a1d94cbfe618
j_a_ <jerome.arfouche@gmail.com>
2019-12-06 08:49:11 -0500

    typography design change

--- site/templates/partials/menu.blade.php
+++ site/templates/partials/menu.blade.php
@@ -1,5 +1,5 @@
 <body class="mw7 center bg-lightest-gray">
-	<nav class="mt5-l pv4 ttl s721-cd">
+	<nav class="mt5-l pv4 ttl s721-cd-light">
 		<a class="link tracked-tight f2 f2-m f1-ns black-70 hover-black-70 di-l db pv2-l ph2 hover-bg-gold" href="{{ url() }}" title="{{ html($site->title()) }}">{{ html($site->title()) }}</a>
 		@foreach($pages->listed() as $p)
 			<a class="{{ e($p->isOpen() && $site->page()->title() != 'cart', 'bb b--gold bw2') }} {{ e($loop->first, 'pa2', 'pa1') }} link gray f5 f4-m f3-ns dib pv0 ml0-ns pa1-l hover-bg-gold hover-white" href="{{ $p->url() }}" title="{{ $p->title() }}">{{ $p->title() }}</a>
