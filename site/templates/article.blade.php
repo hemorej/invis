727ad5e1c413f53b26fe7374ef5d94a4055f066a
j_a_ <jerome.arfouche@gmail.com>
2020-08-30 18:28:52 -0400

    minor template patch

--- site/templates/article.blade.php
+++ site/templates/article.blade.php
@@ -74,15 +74,15 @@
 	@endphp
 	@if($page->hasPrevListed($articles))
 		<p class="fl">
-			<a class="f5 f4-m f4-ns pa1-l link black-60 hover-white hover-bg-gold" href="{{ $page->prev($articles)->url() }}">&laquo; {{ $page->parent()->title() == 'journal' ? 'previous' : $page->prev($articles)->title() }}</a><p class="fl">&nbsp;&vert;&nbsp;</p>
+			<a class="f5 f4-m f4-ns pa1-l link black-60 hover-white hover-bg-gold" href="{{ $page->prev($articles)->url() }}">&laquo; {{ $page->parent()->title() == 'journal' ? 'next' : $page->prev($articles)->title() }}</a>
 		</p>
 	@endif
 	@if($page->parent()->title() == 'journal')
-		<p class="fl"><a class="f5 f4-m f4-ns link pa1-l black-60 hover-white hover-bg-gold" href="{{ $page->parent()->url() }}?all=1">all posts</a></p>
+		<p class="fl">&nbsp;&nbsp;<a class="f5 f4-m f4-ns link pa1-l black-60 hover-white hover-bg-gold" href="{{ $page->parent()->url() }}?all=1">all posts</a></p>
 	@endif
 	@if($page->hasNextListed($articles))
 		<p class="fr">
-			<a class="f5 f4-m f4-ns link pa1-l black-60 hover-white hover-bg-gold" href="{{ $page->next($articles)->url() }}">{{ $page->parent()->title() == 'journal' ? 'next' : $page->next($articles)->title() }} &raquo;</a>
+			<a class="f5 f4-m f4-ns link pa1-l black-60 hover-white hover-bg-gold" href="{{ $page->next($articles)->url() }}">{{ $page->parent()->title() == 'journal' ? 'previous' : $page->next($articles)->title() }} &raquo;</a>
 		</p>
 	@endif
 </nav>
