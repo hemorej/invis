8b45f3dfbae211d64be033f7a64271303ff963f5
j_a_ <jerome.arfouche@gmail.com>
2020-09-24 12:36:11 -0400

    cdn integration first draft

--- site/config/config.php
+++ site/config/config.php
@@ -1,48 +1,51 @@
 <?php
 
 return [
-	'env' => 'dev',
+    'env' => 'dev',
 
-	'debug' => true,
-	'cache' => false,
-	'cache.driver' => 'file',
-	'cache.autoupdate' => true,
-	'cache.ignore' => array(
-	  'home',
-	  'feed',
-	  'prints/*'
-	),
-	'cache.backend' => true,
-	'afbora.blade.minify.enabled' => false,
+    'debug' => true,
+    'cache' => false,
+    'cache.driver' => 'file',
+    'cache.autoupdate' => true,
+    'cache.ignore' => array(
+        'home',
+        'feed',
+        'prints/*'
+    ),
+    'cache.backend' => true,
+    'afbora.blade.minify.enabled' => false,
 
-	'thumbs' => [
-    	'quality'   => 90,
-    	'srcsets' => [
+    'thumbs' => [
+        'quality'   => 90,
+        'srcsets' => [
             'default' => [600, 800, 1200],
             'vertical' => [ '400w' => ['height' => 600], '600w' => ['height' => 600], '800w' => ['height' => 600]]
         ]
     ],
 
-	'mailgun_domain' => '',
-	'mailgun_key' => '',
-	'alert_address' => '',
-	'from_address' => '',
-	'reply-to_address' => '',
+    'mailgun_domain' => '',
+    'mailgun_key' => '',
+    'alert_address' => '',
+    'from_address' => '',
+    'reply-to_address' => '',
 
-	'stripe_key_pub' => '',
-	'stripe_key_prv' => '',
+    'stripe_key_pub' => '',
+    'stripe_key_prv' => '',
     'webhook_secret' => '',
-	'paypal_client_id' => '',
-	'paypal_client_secret' => '',
-	'paypal_environment' => '',
-	'fixer_key' => '',
-	'ipstack_key' => '',
+    'paypal_client_id' => '',
+    'paypal_client_secret' => '',
+    'paypal_environment' => '',
+    'fixer_key' => '',
+    'ipstack_key' => '',
 
     'umami_code' => '',
 
-	'cacheTTL' => '14400',
+    'cacheTTL' => '14400',
 
-	'bnomei.autoid.generator' => function(){ return Bnomei\AutoID::getToken(15, true, true, true); },
+    'keycdn' => false,
+    'keycdn.domain' => '',
 
-	'hooks' => require_once 'hooks.php'
+    'bnomei.autoid.generator' => function(){ return Bnomei\AutoID::getToken(15, true, true, true); },
+
+    'hooks' => require_once 'hooks.php'
 ];
---
+++ site/plugins/keycdn/index.php
@@ -0,0 +1,74 @@
+<?php
+
+use Kirby\Cms\File;
+use Kirby\Cms\FileVersion;
+use Kirby\Http\Uri;
+use Kirby\Toolkit\Str;
+
+function keycdn($file, $params = [])
+{
+    $suffix = null;
+    $name = Str::before($file->filename(), "." . $file->extension());
+
+    // check the parameters passed to the function and set width and height
+    if (empty($params) === false) {
+        if (empty($params['height']) === false) {
+            $suffix = '-x' . $params['height'];
+        }elseif($params['width'] !== false) {
+            $suffix = '-' . $params['width'] . 'x';
+        }
+    }
+    $versionName = Str::replace($name . $suffix . '.' . $file->extension(), '_', '-');
+
+    // strip site url and replace previous filename from old path with new version name
+    $newFilename = Str::after(Str::before($file->mediaUrl(), $file->filename()), kirby()->site()->url()) . $versionName;
+
+    // return final URL
+    return option('keycdn.domain') . $newFilename;
+}
+
+Kirby::plugin('author/keycdn', [
+    'components' => [
+        'url' => function ($kirby, $path, $options, $original) {
+            if (option('keycdn', false) !== false && Str::contains($path, 'assets')) {
+                return option('keycdn.domain') . '/' . Cachebuster::path($path);
+            }
+
+            return $original($path, $options);
+        },
+        'file::version' => function (Kirby $kirby, File $file, array $options = []) {
+            static $originalComponent;
+            // if keycdn option is enabled
+            if (option('keycdn', false) !== false) {
+                $url = keycdn($file, $options);
+                // return a new FileVersion object with the given settings
+                return new FileVersion([
+                    'modifications' => $options,
+                    'original'      => $file,
+                    'root'          => $file->root(),
+                    'url'           => $url,
+                ]);
+            }
+            // if $originalComponent is null, require the original component
+            if ($originalComponent === null) {
+                $originalComponent = (require $kirby->root('kirby') . '/config/components.php')['file::version'];
+            }
+            // and return it with the given options
+            return $originalComponent($kirby, $file, $options);
+        },
+        'file::url' => function (Kirby $kirby, File $file): string {
+            static $originalComponent;
+            // if the file type is an image
+            if ($file->type() === 'image' && option('keycdn', false) !== false) {
+                // call the keycdn method
+                return keycdn($file);
+            }
+            // if $originalComponent is null, require the original component
+            if ($originalComponent === null) {
+                $originalComponent = (require $kirby->root('kirby') . '/config/components.php')['file::url'];
+            }
+            // and return it
+            return $originalComponent($kirby, $file);
+        }
+    ]
+]);
