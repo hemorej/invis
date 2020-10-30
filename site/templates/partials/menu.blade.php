9e9c2c9af90fd2cade3f71259325275a323806c9
j_a_ <jerome.arfouche@gmail.com>
2020-10-30 07:59:46 -0400

    responsive fixes. product desc bugfix with fixed layout

--- site/templates/partials/menu.blade.php
+++ site/templates/partials/menu.blade.php
@@ -2,10 +2,10 @@
 	<nav class="mt5-l pv4 ttl s721-cd-light">
 		<a class="link tracked-tight f2 f2-m f1-ns black-70 hover-black-70 di-ns db pv2-l ph2 hover-bg-gold mr3" href="{{ url() }}" title="{{ html($site->title()) }}">{{ html($site->title()) }}</a>
 		@foreach($pages->listed() as $p)
-			<a class="{{ e($p->isOpen() && $site->page()->title() != 'cart', 'bb b--gold bw2') }} {{ e($loop->first, 'pa2', 'pa1') }} link gray f5 f4-m f3-ns dib pv0 ml0-ns pa1-l hover-bg-gold hover-white" href="{{ $p->url() }}" title="{{ $p->title() }}">{{ $p->title() }}</a>
+			<a class="{{ e($p->isOpen() && $site->page()->title() != 'cart', 'bb b--gold bw2') }} {{ e($loop->first, 'pa2', 'pa1') }} link gray f4 f3-ns dib pv0 ml0-ns pa1-l hover-bg-gold hover-white" href="{{ $p->url() }}" title="{{ $p->title() }}">{{ $p->title() }}</a>
 		@endforeach
 
 		@if(!empty(kirby()->session()->get('txn')))
-			<a class="{{ e($site->page()->title() == 'cart', 'bb b--gold bw2') }} pa1 link gray f5 f4-m f3-ns dib pv0 ml0-ns pa2-l hover-bg-gold hover-white" href="/prints/cart" title="cart">cart</a>
+			<a class="{{ e($site->page()->title() == 'cart', 'bb b--gold bw2') }} pa1 link gray f4 f3-ns dib pv0 ml0-ns pa2-l hover-bg-gold hover-white" href="/prints/cart" title="cart">cart</a>
 		@endif
 	</nav>
--- site/templates/product.blade.php
+++ site/templates/product.blade.php
@@ -19,69 +19,67 @@
 </noscript>
 
 <div id="prod" class="black-70 ph2">
-    <span class="f5 f4-m f3-ns black-70 db mb3">{{ $page->parent()->title() | lower }}&nbsp;<a class="f5 f4-m f3-ns link black-60 hover-white hover-bg-gold pa1" href="{{ $page->url() }}">{{ $page->title() | lower }}</a></span>
+    <span class="f4 f3-ns black-70 db mb3">{{ $page->parent()->title() | lower }}&nbsp;<a class="f4 f3-ns link black-60 hover-white hover-bg-gold pa1" href="{{ $page->url() }}">{{ $page->title() | lower }}</a></span>
 
-    <div class="mw9 center">
-        <div class="cf">
-            <div class="fl w-100 w-50-ns">
-                @foreach($page->images() as $image)
-                    <img class="db mb4" alt="product pictures for {{ $page->title() }}" srcset="{{ $image->srcset([600, 800, 1200]) }}">
-                @endforeach
-            </div>
-        
-            <div class="fl w-100 w-50-ns pa4-ns">
-                <section class="variants fixed-ns static mw6-ns">
-                    @php
-                    $variants = $page->variants()->toStructure();
+    <div class="cf">
+        <div class="fl w-100 w-50-ns">
+            @foreach($page->images() as $image)
+                <img class="db mb4" alt="product pictures for {{ $page->title() }}" srcset="{{ $image->srcset([600, 800, 1200]) }}">
+            @endforeach
+        </div>
+    
+        <div class="fl w-100 w-50-ns pa4-ns">
+            <section class="variants fixed-l mw6-ns">
+                @php
+                $variants = $page->variants()->toStructure();
 
-                    $stock = 0;
-                    foreach($variants as $variant)
-                        $stock += $variant->stock()->value();
+                $stock = 0;
+                foreach($variants as $variant)
+                    $stock += $variant->stock()->value();
 
-                    @endphp
-                    @if(count($variants) == 0 || $stock == 0)
-                        Out of stock
-                    @else
-                        <ul class="list pv2 pl0">
-                            @foreach($variants as $variant)
-                                @if(Cart::inStock($variant))
-                                    <li class='{{ e($loop->first, 'dib pl0', 'dib pt3') }}'>
-                                    <a {{ e($loop->first, 'ref="active"') }} 
-                                    class="f4 link black-60 hover-white hover-bg-gold pa1-l {{ e($loop->first, 'bb b--gold bw2')}}"
-                                    data-option-variant='{{ $variant->autoid() }}'
-                                    data-option-product='{{ $page->title() . $variant->name() }}'
-                                    v-on:click.prevent='makeActive'>
-                                        {{ $variant->name() }} &mdash; ${{ $variant->price() }}
-                                    </a>
-                                    </li>&nbsp;
-                                @endif
-                            @endforeach
-                        </ul>
+                @endphp
+                @if(count($variants) == 0 || $stock == 0)
+                    Out of stock
+                @else
+                    <ul class="list pv2 pl0">
+                        @foreach($variants as $variant)
+                            @if(Cart::inStock($variant))
+                                <li class='{{ e($loop->first, 'dib pl0', 'dib pt3') }}'>
+                                <a {{ e($loop->first, 'ref="active"') }} 
+                                class="f4 link black-60 hover-white hover-bg-gold pa1-l {{ e($loop->first, 'bb b--gold bw2')}}"
+                                data-option-variant='{{ $variant->autoid() }}'
+                                data-option-product='{{ $page->title() . $variant->name() }}'
+                                v-on:click.prevent='makeActive'>
+                                    {{ $variant->name() }} &mdash; ${{ $variant->price() }}
+                                </a>
+                                </li>&nbsp;
+                            @endif
+                        @endforeach
+                    </ul>
 
-                        <form id="cart-form" method="post" action="">
-                            <div class="description">
-                                <input type="hidden" name="csrf" ref="csrf" value="@csrf()">
-                                <input type="hidden" name="action" value="add">
-                                <input type="hidden" name="uri" ref="uri" value="{{ $page->uri() }}">
-                            </div>
+                    <form id="cart-form" method="post" action="">
+                        <div class="description">
+                            <input type="hidden" name="csrf" ref="csrf" value="@csrf()">
+                            <input type="hidden" name="action" value="add">
+                            <input type="hidden" name="uri" ref="uri" value="{{ $page->uri() }}">
+                        </div>
 
-                            <div class="action mb4">
-                                <button 
-                                    :disabled="submitting == true"
-                                    v-on:click.prevent='addToCart'
-                                    class="bg-white f5 no-underline" 
-                                    :class="[submitting == true ? 'gray b--gray pa2 pa3-l' : 'black bg-animate b--gold pa2 pa3-l ba border-box']">
-                                    <span v-if="submitting == true">adding&ensp;&hellip;</span>
-                                    <span v-else>add to cart</span>
-                                </button>
-                            </div>
-                        </form>
-                        <span class="measure-narrow lh-copy black-70 f4">
-                            {!! $page->description()->kirbytext() !!}
-                        </span>
-                    @endif
-                </section>
-            </div>
+                        <div class="action mb4">
+                            <button 
+                                :disabled="submitting == true"
+                                v-on:click.prevent='addToCart'
+                                class="bg-white f5 no-underline" 
+                                :class="[submitting == true ? 'gray b--gray pa2 pa3-l' : 'black bg-animate b--gold pa2 pa3-l ba border-box']">
+                                <span v-if="submitting == true">adding&ensp;&hellip;</span>
+                                <span v-else>add to cart</span>
+                            </button>
+                        </div>
+                    </form>
+                    <span class="measure-narrow lh-copy black-70 f4">
+                        {!! $page->description()->kirbytext() !!}
+                    </span>
+                @endif
+            </section>
         </div>
     </div>
 </div>
