0258d40202858fdfc5c82738847f31089e124823
hemorej <hemorej@users.noreply.github.com>
2023-01-20 23:40:39 +0000

    Update config.php

--- site/config/config.php
+++ site/config/config.php
@@ -8,7 +8,6 @@ return [
         'pages' => [
             'active'  => true,
             'type'    => 'static',
-            'root'    => __DIR__ . '/../cache/',
             'headers' => true,
             'ignore' => [
                 'home',
