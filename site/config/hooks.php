9a9ec6066527e5b57d3b80bbea270cd3ee0c170b
j_a_ <jerome.arfouche@gmail.com>
2023-03-12 13:20:51 -0400

    bugfix: journal single vertical img oversized

--- site/config/hooks.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once(__DIR__ . '/../models/shippingHandler.php');
-
-return [
-  'page.update:after' => function ($newPage, $oldPage = null) {
-  	$shippingHandler = new \ShippingHandler();
-    $shippingHandler->handle($newPage, $oldPage);
-  }
-];
