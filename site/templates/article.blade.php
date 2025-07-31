d7ffe7f87c61d4d11bf8db6941f47a867824b4a9
j_a_ <jerome.arfouche@pm.me>
2025-07-31 11:55:25 -0400

    journal template vertical image bugfix

--- site/templates/article.blade.php
+++ site/templates/article.blade.php
@@ -65,7 +65,7 @@
 				@endif
 
 					@if($current->isPortrait() && count($page->images()) == 1)
-						<img style="max-width: 50%" alt="{{$headline}}" class="lazy" data-srcset="{{ $current->srcset('vertical') }}">
+						<img style="max-width: 45%" alt="{{$headline}}" class="lazy" data-srcset="{{ $current->srcset('vertical') }}">
 					@elseif($current->isPortrait())
 						<img alt="{{$headline}}" class="lazy" data-srcset="{{ $current->srcset('vertical') }}">
 					@else
