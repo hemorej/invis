9e7f0a9b5aab3ddd9c395d60ea494fb040601a70
j_a_ <jerome.arfouche@pm.me>
2026-06-26 09:34:04 -0400

    finish removing discount feature

--- site/models/ShippingHandler.php
+++ site/models/ShippingHandler.php
@@ -44,16 +44,8 @@ class ShippingHandler
 				$subtotal += intval( $item->quantity()->value * $item->amount()->value );
 			}
 
-			$discount = $page->discount()->yaml();
-			$shipping = $page->shipping()->value;
-
-			if( !empty( $discount ) ) {
-				$total = $subtotal - ( intval( $discount['amount'] ) / 100 ) * $subtotal;
-			} else {
-				$total = $subtotal;
-			}
-
-			$total += $shipping;
+			$shipping = (float) $page->shipping()->value;
+			$total = $subtotal + $shipping;
 
 			try {
 				$mailbun = new Mailbun();
@@ -68,8 +60,6 @@ class ShippingHandler
 					'country' => $customer['address']['country'],
 					'postcode' => $customer['address']['postal_code'],
 					'email' => $customer['email'],
-					'discount' => empty( $discount['code'] ) ? null : $discount['code'],
-					'discountAmount' => empty( $discount['amount'] ) ? null : $discount['amount'],
 					'shipping' => $shipping,
 					'total' => $total,
 					'title' => 'Your order from The Invisible Cities has been shipped',
@@ -80,8 +70,8 @@ class ShippingHandler
 				] );
 
 				$this->logger->info( "email shipping confirmation sent for order id " . $page->suuid() );
-			} catch( \Error $e ) {
-				$this->logger->error( "email shipping confirmation error for order id " . $page->suuid() . ": " . $e->getMessage() );
+			} catch( \Throwable $t ) {
+				$this->logger->error( "email shipping confirmation error for order id " . $page->suuid() . ": " . $t->getMessage() );
 			}
 
 			$page->update( [
