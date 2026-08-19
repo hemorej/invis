3db0c98e8ccdafc93c38dbac0678139e1e318cc8
j_a_ <jerome.arfouche@pm.me>
2026-08-19 14:22:59 -0400

    remove homepage link

--- site/templates/home.php
+++ site/templates/home.php
@@ -8,8 +8,6 @@
 
 <?php snippet('partials/menu') ?>
 
-<a href="<?= html($pages->listed()->first()->url()) ?>" class="db">
-	<img alt="black and white photograph" class="db w-100" width="<?= $heroImage->width() ?>" height="<?= $heroImage->height() ?>" style="height:auto" sizes="100vw" fetchpriority="high" srcset="<?= html($heroImage->srcset([600, 800, 1200])) ?>">
-</a>
+<img alt="black and white photograph" class="db w-100" width="<?= $heroImage->width() ?>" height="<?= $heroImage->height() ?>" style="height:auto" sizes="100vw" fetchpriority="high" srcset="<?= html($heroImage->srcset([600, 800, 1200])) ?>">
 
 <?php snippet('partials/footer') ?>
