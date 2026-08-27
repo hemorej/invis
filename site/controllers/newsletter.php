0be83057a9bd4358695b09ed9f9f3e70ccd6ca00
j_a_ <jerome.arfouche@pm.me>
2026-08-27 13:15:23 -0400

    added comments

--- site/controllers/newsletter.php
+++ site/controllers/newsletter.php
@@ -1,12 +1,25 @@
 <?php
 
+/**
+ * Controller for the newsletter template.
+ *
+ * Pulls the one-shot flash notice set by the newsletter/subscribe,
+ * /confirm and /unsubscribe routes (see site/plugins/newsletter/index.php),
+ * clears it from the session, and hands it to the template so the outcome
+ * of the last action is shown exactly once.
+ *
+ * @param \Kirby\Cms\Site $site
+ * @param \Kirby\Cms\Page $page
+ * @param \Kirby\Cms\App  $kirby
+ * @return array{notice: array{type: string, message: string}|null}
+ */
 return function ( $site, $page, $kirby ) {
 	$session = $kirby->session();
 	$notice = null;
 
 	if( $session->get( 'notice' ) ) {
 		$notice = $session->get( 'notice' );
-		$session->remove( 'notice' );
+		$session->remove( 'notice' ); // flash: consume on first read
 	}
 
 	return ['notice' => $notice];
